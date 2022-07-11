c61b283dbff86ef04d8aec18ecccd0de37edf860
Your Name <xi03hy@gmail.com>
2022-07-11 23:23:01 +0900

    제발

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Main = () => {
     return (
         <div>
-            <h1>하이</h1>
+            <h1>대체 왜 나는 걸까 오류가?</h1>
+            <p>나도모르겠다</p>
+            
         </div>
     );
 };
