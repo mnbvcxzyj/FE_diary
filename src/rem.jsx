5cd8f1e97cce138934ed1996f650aa906d2cd621
Your Name <xi03hy@gmail.com>
2022-07-11 23:46:48 +0900

    또 바꿔봤습니다

--- src/rem.jsx
+++ src/rem.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const rem = () => {
     return (
         <div>
-           추억보기 페이지 입니다 다시 실행해보겠습니다 헤헤
-           <p> 음음 </p>
+          <h1>하이</h1>
         </div>
     );
 };
