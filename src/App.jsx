79894055d06222825fb5d211eb5860e3530969d8
Your Name <xi03hy@gmail.com>
2022-07-11 23:11:23 +0900

    다시 고침

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const Main = () => {
+const App = () => {
     return (
         <div>
-            안녕하세요 실험해보겠습니다.!!
-            하하하
+            <h1>하이</h1>
         </div>
     );
 };
 
-export default Main;
+export default App;
--- src/rem.jsx
+++ src/rem.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const rem = () => {
     return (
         <div>
-           추억보기 페이지 입니다 다시 실행해보겠습니다
+           추억보기 페이지 입니다 다시 실행해보겠습니다 헤헤
+           <p> 음음 </p>
         </div>
     );
 };
