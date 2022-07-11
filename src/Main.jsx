18596d3d17d43493c6c8095a6519f98df42391cd
Your Name <xi03hy@gmail.com>
2022-07-11 23:24:45 +0900

    change

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
 const Main = () => {
-    return (
-        <div>
-            <h1>대체 왜 나는 걸까 오류가?</h1>
-            <p>나도모르겠다</p>
-            
-        </div>
-    );
+    return <div>Main</div>
 };
 
 export default Main;
