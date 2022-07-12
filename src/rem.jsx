ac44382df5405992bbac04ad87696434b56342c8
Your Name <xi03hy@gmail.com>
2022-07-12 17:42:09 +0900

    calender 기능 구현 (1차)

--- src/rem.jsx
+++ src/rem.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
-const rem = () => {
+const CalendarContents = styled.div`
+  position: absolute;
+  width: 444px;
+  height 480px;
+  margin-top: 156px;
+  margin-left: 184px;
+  border: 2px solid #9B8962;
+  border-radius: 5px;
+`;
+
+
+const CalendarName = styled.div`
+  margin-right; 150px;
+`;
+
+const Rem = () => {
+    const [value, onChange] = useState(new Date());
+
     return (
-        <div>
-          <h1>하이</h1>
-        </div>
+        <>
+            <CalendarName>하루의 같이가 모여 만든 가치</CalendarName>
+            <CalendarContents>
+                <Calendar
+                  onChange={onChange}
+                  value={value}
+                />
+            </CalendarContents>
+        </>
     );
 };
 
-export default rem;
+export default Rem;
