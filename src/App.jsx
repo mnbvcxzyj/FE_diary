50cb3d3b526628c52ec471401e40ddfc9d860466
Yeji Kim <mnbvcxzyj@gmail.com>
2022-07-15 00:31:52 +0900

    Header 포함 App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useRef } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from "./style";
+import Header from "./Header";
+import Main from "./Main";
 
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+      <Main />
+    </>
   );
 };
 
-export default App; 
+export default App;
