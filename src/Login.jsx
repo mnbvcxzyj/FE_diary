7699c43e4ff3153e23d24ca86f99cd828ea4aee5
dbsrudwkd <76401181+dbsrudwkd@users.noreply.github.com>
2022-07-15 02:40:00 +0900

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,7 @@
-import React from "react";	
+import React from "react";
 
 function Login(props) {	
   return <div>여기는 로그인 화면이 들어갈 예정입니다. github test용</div>;	
 }	
 
-export default Login;	
+export default Login;
