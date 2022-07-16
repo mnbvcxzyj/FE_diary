f300ac6a23263a45c367f7d132da95e12bcf5f8b
yunkyeong Jang <alzkdpffk4314@gmail.com>
2022-07-16 17:43:41 +0900

    CommnetEditor-댓글 작성 버튼

--- src/CommnetEditor.jsx
+++ src/CommnetEditor.jsx
@@ -1,61 +1,88 @@
 import {useRef, useState} from "react";
+import { 
+    CommentButton,
+    CommentDiv,
+    EnterImg,
+    InputNames,
+    InputText,
+    CommnetSubmit,
+    CommentWrote,
+} from './CommentInput';
+import CommentUpLoad from './img/commentUpLoad.png';
+import Enter from './img/Enter.png'
+import commentButton from './img/CommentButton.png';
 
 const CommentEditor = ({onCreate}) => {
-
-    const authorInput = useRef();
     const contentInput = useRef();
-
     const [state, setState] = useState({
-        author:"", 
-        content:"",
+        content: "",
     }); 
 
     const handleChangeState = (e) => {
         setState({
             ...state,
-            [e.target.name]: e.target.value,
-        });
+            "content": e.target.value,
+    });
     };
 
     const handleSubmit = () => {
-        if(state.author.length < 1){
-            authorInput.current.focus();
-            return;
-        }
-
-        if(state.content.length < 1){
-            contentInput.current.focus();
-            alert("작성된 내용이 없습니다. ")
-            return;
-        }
-
-        onCreate(state.author, state.content, state.emotion);
-        alert("댓글이 작성되었습니다!")
-    }
+        onCreate(state.content);
+    };
 
     return(
         <div className = "CommentEditor">
-        <div>
-            <input
-                ref={authorInput}
-                name="author" 
-                type="text"
-                value={state.author} 
-                onChange={handleChangeState}
+        <CommentButton>
+            <img src={commentButton}
             />
+        </CommentButton>
+        <CommentDiv>
+        <CommentWrote>
+        {/* <>
+        <div className="CommentItem">
+            <div className="info">
+                <CommentWriter1>사용자1</CommentWriter1>
+            </div>
+            <CommentTextWriter> 안녕!!</CommentTextWriter>
+        <div></div>
         </div>
+        <div className="CommentItem">
+            <div className="info">
+                <CommentWriter>사용자2</CommentWriter>
+            </div>
+            <CommentTextWriter>침대 최고야</CommentTextWriter>
+        <div></div>
+        </div>
+        </> */}
+        </CommentWrote>
         <div>
-            <textarea
+            <EnterImg>
+                <div>
+                <img src={Enter}
+                    width='24px'
+                    height='24px'
+                    />
+                </div>
+            </EnterImg>
+            <InputNames
+            >사용자2</InputNames>
+            <InputText
                 ref={contentInput}
                 name="content"
-                type="text"
-                value={state.content} 
+                type="text" 
                 onChange={handleChangeState}
             />
         </div>
         <div>
-            <button onClick={handleSubmit}>추가하기</button>
+            <button onClick={handleSubmit}>
+            <CommnetSubmit>
+                    <img src={CommentUpLoad}
+                    width='19px'
+                    height='19px'
+                    />
+            </CommnetSubmit>
+            </button>
         </div>
+        </CommentDiv>
     </div>
     );
 };
