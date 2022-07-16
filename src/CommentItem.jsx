75dc6ee4ad52cb2e0be8aceb973e3d139b6681a6
yunkyeong Jang <alzkdpffk4314@gmail.com>
2022-07-16 17:37:30 +0900

    CommitItem-댓글작성자+내용 출력

--- src/CommentItem.jsx
+++ src/CommentItem.jsx
@@ -1,14 +1,24 @@
-const CommentItem = ({author, content, created_date, emotion, id}) => {
+import { 
+    CommentButton,
+    CommentDiv,
+    EnterImg,
+    InputNames,
+    InputText,
+    CommnetSubmit,
+    CommentWrote,
+    CommentWriter,
+    CommentTextWriter,
+    CommentWriter1,
+} from './CommentInput';
+import CommentUpLoad from './img/commentUpLoad.png';
+import Enter from './img/Enter.png'
+import commentButton from './img/CommentButton.png';
+
+const CommentItem = ({content}) => {
     return (
-        <div className="DiaryItem">
-            <div className="info">
-                <span>
-                    {author}
-                </span>
-                <br/>
-            </div>
-            <div className="content">{content}<br/><hr/></div>
-        <div></div>
+        <div className="CommentItem">
+            <CommentWriter>사용자2</CommentWriter>
+            <CommentTextWriter>{content}</CommentTextWriter>
         </div>
     );
 };
