90c9e01ba8364c70e24c2d7431a0d162f8abe254
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-07-16 12:31:01 +0900

    Revert "Yun kyeong"

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,11 @@
-import React, {useState} from 'react';
-import { GlobalStyle } from "./style";
-import Header from './Header';
-import CommentInput from './CommentInput';
-import { CommentBtn } from './CommentInput';
-import commentButton from './img/CommentButton.png';
-import Comment from './Comment';
+import React from 'react';
 
 const App = () => {
-  const [commentOn, setCommentOn] = useState(false);
-
-  
   return (
-    <>
-    {/* <CommentInput
-        show={commentOn} 
-        onHide={()=>setCommentOn(false)}
-      />
-    <GlobalStyle/>
-    <Header/>     
     <div>
-      <CommentBtn onClick={()=>setCommentOn(true)}>
-        <img src={commentButton}/>
-      </CommentBtn>
-    </div> */}
-    <Header></Header>
-    {/*<Comment/>*/}
-    </>
+
+    </div>
   );
 };
 
-export default App;  
+export default App; 
