efb93d21a0796f54d3449d5eb2e2b794bab8ea1c
yunkyeong Jang <alzkdpffk4314@gmail.com>
2022-07-16 17:40:10 +0900

    CommentList-댓글 배열로 출력

--- src/CommentList.jsx
+++ src/CommentList.jsx
@@ -1,14 +1,25 @@
 import CommentItem from "./CommentItem";
+import { 
+    CommentWriter,
+    CommentWriter1,
+    CommentTextWriter,
+} from "./CommentInput";
 
 const CommentList = ({commentList}) => {
+    console.log(commentList)
     return (
-        <div className='CommentList'>
-            <div className="CommentBlock">
-                {commentList.map((it) => (
-                    <CommentItem key={it.id} {...it}/>
-                ))}
-            </div>
-            <div></div>
+        <div>
+        {/*더미데이터...?*/}
+        <>
+        <CommentWriter1>사용자1</CommentWriter1>
+        <CommentTextWriter> 안녕!!</CommentTextWriter>
+       
+        <CommentWriter>사용자2</CommentWriter>
+        <CommentTextWriter>침대 최고야</CommentTextWriter>
+        </>
+            {commentList.map((it) => (
+                <CommentItem key={it.id} {...it}/>
+            ))}
         </div>
     );
 };
